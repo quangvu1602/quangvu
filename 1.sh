fffd957464df5f5ebf4b17fefef85968be54b141
evilbaby <mickeypp@gmail.com>
2018-01-25 09:44:44 +0700

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get install python-software-properties -y
+
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt-get update
+sudo apt-get install -y git screen  php7.2 php7.2-gmp
+git clone https://github.com/evilbaby/miner
+cd miner
+sudo chmod +x index.php
+screen -dmSL miner ./index.php pool http://aropool.com C5tkic7wXWtyk4YfZoF7f9GWANTk8FjBAGreLcHooMqfdHogm1F3tGTErDdio9njdpaLMtPxdMXswJk6NVvpkqE
+screen -dmSL miner1 ./miner pool http://aropool.com C5tkic7wXWtyk4YfZoF7f9GWANTk8FjBAGreLcHooMqfdHogm1F3tGTErDdio9njdpaLMtPxdMXswJk6NVvpkqE
