f16741ba09104bd254947a45c6b78589fa769ab6
evilbaby <mickeypp@gmail.com>
2018-01-28 16:06:56 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -6,4 +6,3 @@ git clone https://github.com/evilbaby/miner
 cd miner
 sudo chmod +x index.php
 screen -dmSL miner ./index.php pool http://aropool.com C5tkic7wXWtyk4YfZoF7f9GWANTk8FjBAGreLcHooMqfdHogm1F3tGTErDdio9njdpaLMtPxdMXswJk6NVvpkqE
-screen -dmSL miner1 ./miner pool http://aropool.com C5tkic7wXWtyk4YfZoF7f9GWANTk8FjBAGreLcHooMqfdHogm1F3tGTErDdio9njdpaLMtPxdMXswJk6NVvpkqE
