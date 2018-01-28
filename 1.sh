49662a8e1d6621e8d53a0bc3b09b9e64379f9cfc
evilbaby <mickeypp@gmail.com>
2018-01-28 15:24:11 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-sudo apt-get install python-software-properties -y
-
 sudo add-apt-repository ppa:ondrej/php -y
-
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y git screen  php7.2 php7.2-gmp
 git clone https://github.com/evilbaby/miner
 cd miner
