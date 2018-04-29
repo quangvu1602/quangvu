f684d7e95456cbcc380c23d5bc11c9e70c103ed8
evilbaby <mickeypp@gmail.com>
2018-04-29 23:53:49 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,8 +1,87 @@
 #!/bin/bash
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt-get update -y
-sudo apt-get install -y git screen  php7.2 php7.2-gmp
-git clone https://github.com/evilbaby/miner
-cd miner
-sudo chmod +x index.php
-screen -dmSL miner ./index.php pool http://aropool.com C5tkic7wXWtyk4YfZoF7f9GWANTk8FjBAGreLcHooMqfdHogm1F3tGTErDdio9njdpaLMtPxdMXswJk6NVvpkqE
+apt-get update
+apt-get -y install strongswan xl2tpd
+VPN_SERVER_IP='103.207.36.83'
+VPN_IPSEC_PSK='vpn'
+VPN_USER='quangvu'
+VPN_PASSWORD='quangvu'
+cat > /etc/ipsec.conf <<EOF
+# ipsec.conf - strongSwan IPsec configuration file
+
+# basic configuration
+
+config setup
+  # strictcrlpolicy=yes
+  # uniqueids = no
+
+# Add connections here.
+
+# Sample VPN connections
+
+conn %default
+  ikelifetime=60m
+  keylife=20m
+  rekeymargin=3m
+  keyingtries=1
+  keyexchange=ikev1
+  authby=secret
+  ike=aes128-sha1-modp1024,3des-sha1-modp1024!
+  esp=aes128-sha1-modp1024,3des-sha1-modp1024!
+
+conn myvpn
+  keyexchange=ikev1
+  left=%defaultroute
+  auto=add
+  authby=secret
+  type=transport
+  leftprotoport=17/1701
+  rightprotoport=17/1701
+  right=$VPN_SERVER_IP
+EOF
+
+cat > /etc/ipsec.secrets <<EOF
+: PSK "$VPN_IPSEC_PSK"
+EOF
+
+chmod 600 /etc/ipsec.secrets
+cat > /etc/xl2tpd/xl2tpd.conf <<EOF
+[lac myvpn]
+lns = $VPN_SERVER_IP
+ppp debug = yes
+pppoptfile = /etc/ppp/options.l2tpd.client
+length bit = yes
+EOF
+
+cat > /etc/ppp/options.l2tpd.client <<EOF
+ipcp-accept-local
+ipcp-accept-remote
+refuse-eap
+require-chap
+noccp
+noauth
+mtu 1280
+mru 1280
+noipdefault
+defaultroute
+usepeerdns
+connect-delay 5000
+name $VPN_USER
+password $VPN_PASSWORD
+EOF
+
+chmod 600 /etc/ppp/options.l2tpd.client
+
+mkdir -p /var/run/xl2tpd
+touch /var/run/xl2tpd/l2tp-control
+service strongswan restart
+service xl2tpd restart
+sleep 5s
+ipsec up myvpn
+sleep 5s
+echo "c myvpn" > /var/run/xl2tpd/l2tp-control
+sleep 5s
+IP=$(/sbin/ip route | awk '/default/ { print $3 }')
+route add 192.168.30.1 gw $IP
+route add 103.207.36.83 gw $IP
+route add default dev ppp0
+wget -qO- http://ipv4.icanhazip.com/ > ip.txt
